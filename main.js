6ff2ce53bbeaabe85ea4bf31ce853ec7517eff22
Amr-Ali02 <83447653+Amr-Ali02@users.noreply.github.com>
2023-04-09 02:55:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let secs = document.querySelector(".count span:nth-child(4)");
 
 setInterval(() => {
   let current = new Date();
-  let future = new Date("11 19 2022");
+  let future = new Date("11 4 2023");
   let diff = (future - current) / 1e3 / 60 / 60 / 24;
   let updays = Math.floor(diff);
   let uphours = (diff - updays) * 24;
